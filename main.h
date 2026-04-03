844780ed8f591a0ec3ae73e7c3641a92ee238161
n573 <ncauwet@gmail.com>
2026-04-03 10:54:22 -0700

    added string

--- main.h
+++ main.h
@@ -2,4 +2,9 @@
  * The purpose of this file is to create chaos
  * practicing git merges on divergent branches
  *
- * /
+ */
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+char* test = "TESTING TESTING 123!";
